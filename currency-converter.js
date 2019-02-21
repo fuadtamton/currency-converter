4ae7c981c99db7cafd2bd5112e066827312724c5
fuadtamton <fuadt@factweavers.com>
2019-02-21 14:02:51 +0530

    completed

--- currency-converter.js
+++ currency-converter.js
@@ -1,26 +1,27 @@
 const Axios = require("axios");
 
 const getExchangeRate = async (from, to) => {
-    const response = await Axios.get('http://data.fixer.io/api/latest?access_key=a2ab0d3fe47da5db6d49dcff6bd5073f&format=1')
-    const euro = 1 / response.data.rates[from]
-    const rate = euro * response.data.rates[to]
-    return rate;
+    try {
+        const response = await Axios.get('http://data.fixer.io/api/latest?access_key=a2ab0d3fe47da5db6d49dcff6bd5073f&format=1')
+        const euro = 1 / response.data.rates[from]
+        const rate = euro * response.data.rates[to]
+        if (isNaN(rate)) {
+            throw new Error()
+        }
+        return rate;
+    } catch (e) {
+        throw new Error(`Unable get exchange rate for ${from} to ${to}.`)
+    }
 }
 
 const getCountries = async (currencyCode) => {
-    const response = await Axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
-    return response.data.map(country => country.name)
+    try {
+        const response = await Axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
+        return response.data.map(country => country.name)
+    } catch (e) {
+        throw new Error(`Unable to get countries that use ${currencyCode}`)
+    }
 }
-
-// const convertCurrency = (from, to, amount) => {
-//     let convertedAmount;
-//     return getExchangeRate(from, to).then(rate => {
-//         convertedAmount = (rate * amount).toFixed(2)
-//         return getCountries(to)
-//     }).then(countries => {
-//         return `${amount} ${from} is worth ${convertedAmount} ${to}. You can spend these in the following countries ${countries}`
-//     })
-// }
 
 const convertCurrency = async (from, to, amount) => {
     const rate = await getExchangeRate(from, to)
@@ -31,4 +32,6 @@ const convertCurrency = async (from, to, amount) => {
 
 convertCurrency('USD', 'INR', 25).then(message => {
     console.log(message)
+}).catch(e => {
+    console.log(e)
 })
